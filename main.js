06f55766ec45a95a6b21b22b87154b935b533af6
Corey <coreya@gmail.com>
2020-07-17 22:28:24 +1000

    Testing multiple targets

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ T({
   up: ["docker-compose", "up"],
   down: ["docker-compose", "down"],
 });
+
+T({
+  name: "pico-test-two",
+  url: "https://github.com/digitalapnea/pico-example-target",
+  up: ["docker-compose", "up"],
+  down: ["docker-compose", "down"],
+});
+
+T({
+  name: "pico-test-three",
+  url: "https://github.com/digitalapnea/pico-example-target",
+  up: ["docker-compose", "up"],
+  down: ["docker-compose", "down"],
+});
